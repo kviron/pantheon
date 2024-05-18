b7b07ecdadfacee4bd00651ad3da1244ac6f2ee4
Мамаев Роман <mamaev@myservices.digital>
2024-05-18 22:11:10 +0300

    добавил каркас основного лайота

--- src/renderer/src/app/App.tsx
+++ src/renderer/src/app/App.tsx
@@ -1,10 +1,15 @@
 import { Suspense } from 'react'
 import { AppRouter } from './providers/router'
+import { MainLayout } from '../shared/layouts/MainLayout/MainLayout'
 
 function App() {
   return (
     <Suspense fallback="">
-      <AppRouter />
+      <MainLayout
+        header={<div>Навбра</div>}
+        content={<AppRouter />}
+        sidebar={<div>сайдебар</div>}
+      />
     </Suspense>
   )
 }
